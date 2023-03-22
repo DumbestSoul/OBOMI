d9fb83b9a8b10a02490e9f4462718850561f9928
Rajat Kishore Acharya <rajat.getmeonfb@gmail.com>
2023-03-23 01:12:07 +0530

    Main2 element created

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import Main1 from "./components/Main1";
+import Main2 from './components/Main2';
 
 export default function App(){
     return (
         <div className="mainBody">
             <Main1 />
-            <div className="main2"></div>
+            <Main2 />
         </div>
     )
 }
---
+++ components/Main2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/main2.css';
+
+
+export default function Main2(){
+    return (
+        <div className="main2">
+        </div>
+    );
+}
